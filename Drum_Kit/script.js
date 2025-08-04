e09cfeb8a17be32c9e890ec816225607097550d3
ShayaanRK <kshayaanraza@gmail.com>
2025-08-04 19:55:40 +0530

    Updated the events logic

--- Drum_Kit/script.js
+++ Drum_Kit/script.js
@@ -1,9 +1,50 @@
-var numButtons = document.querySelectorAll(".drum").length;
-
-for(var i=0; i<numButtons; i++){
-   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-      console.log("Button clicked!");
-      var audio = new Audio("sounds/tom-1.mp3");
-      audio.play();
-   })
+// Play drum sound based on key
+function playDrumSound(key) {
+   switch(key){
+      case "w":
+         var tom1 = new Audio("sounds/tom-1.mp3");
+         tom1.play();
+         break;
+      case "a":
+         var tom2 = new Audio("sounds/tom-2.mp3");
+         tom2.play();
+         break;
+      case "s":
+         var tom3 = new Audio("sounds/tom-3.mp3");
+         tom3.play();
+         break;
+      case "d":
+         var tom4 = new Audio("sounds/tom-4.mp3");
+         tom4.play();
+         break;
+      case "j":
+         var crash = new Audio("sounds/crash.mp3");
+         crash.play();
+         break;
+      case "k":
+         var kickbass = new Audio("sounds/kick-bass.mp3");
+         kickbass.play();
+         break;
+      case "l":
+         var snare = new Audio("sounds/snare.mp3");
+         snare.play();
+         break;
+      default:
+         console.log(key + " is not a valid key for this drum kit.");
+   }
 }
+
+// Keypress event listener
+document.addEventListener("keypress", function(event) {
+   var key = event.key;
+   playDrumSound(key);
+});
+
+// Click event listener for drum buttons
+var drumButtons = document.querySelectorAll(".drum");
+drumButtons.forEach(function(button) {
+   button.addEventListener("click", function() {
+      var buttonKey = this.innerHTML.toLowerCase();
+      playDrumSound(buttonKey);
+   });
+});
